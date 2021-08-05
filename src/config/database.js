3b36660ec8338d473c83d2430c294e6f0e01c85d
Angelica <angelica.botazoli-ext@viavarejo.com.br>
2021-08-04 23:20:29 -0300

    aula4

--- src/config/database.js
+++ src/config/database.js
@@ -1,13 +1,26 @@
 // config, a sintaxe esta no module.exports porque o sequelize só entende essa declaracao
 module.exports = {
+    //usando docker
+    // dialect: 'postgres',
+    // host: 'localhost',
+    // username: 'postgres',
+    // password: 'docker',
+    // database: 'sistema',
+    // define: {
+    //     timestamps: true,
+    //     undercored: true, //permite que faça uso de underline dentro das tabelas
+    //     undercoredAll: true,
+    // }
+
+    //usando ElephantSQL
     dialect: 'postgres',
-    host: 'localhost',
-    username: 'postgres',
-    password: 'docker',
-    database: 'sistema',
+    host: 'queenie.db.elephantsql.com',
+    username: 'hxmjxyaw',
+    password: '8OSQ4QNH0wm76p-Nr0E0tdFNCHpiR7iu',
+    database: 'hxmjxyaw',
     define: {
         timestamps: true,
-        undercored: true, //permite que faça uso de underline dentro das tabelas
-        undercoredAll: true,
+        underscored: true, //permite que faça uso de underline dentro das tabelas
+        underscoredAll: true,
     }
 }
---
+++ src/database/migrations/20210805002718-create-users.js
@@ -0,0 +1,45 @@
+'use strict';
+
+module.exports = {
+  //grava
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.createTable('users', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false, //nao permite que o id seja nulo
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      password_hash: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      provider: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: false,
+        allowNull: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      }
+    })
+  },
+
+  //deleta
+  down: async queryInterface => {
+    return queryInterface.dropTable('users')
+  }
+};
