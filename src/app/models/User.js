425f2116ef0fbdda479e13e214f6389d05ba5a19
Angelica <angelica.botazoli-ext@viavarejo.com.br>
2021-08-06 01:37:08 -0300

    aula7

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,4 +1,5 @@
 import Sequelize, { Model } from 'sequelize'
+import bcrypt from 'bcryptjs'
 
 //as models sao usadas na camada logica para acessar as informacoes do bd e retorna via api rest
 class User extends Model {
@@ -6,6 +7,7 @@ class User extends Model {
         super.init({
             name: Sequelize.STRING,
             email: Sequelize.STRING,
+            password: Sequelize.VIRTUAL, //só vai existir na model, nao vai ser salvo no banco
             password_hash: Sequelize.STRING,
             provider: Sequelize.BOOLEAN
         },
@@ -13,6 +15,14 @@ class User extends Model {
                 sequelize, //referencia o init
                 // tableName: 'users'
             })
+
+        this.addHook('beforeSave', async user => {
+            if (user.password) {
+                user.password_hash = await bcrypt.hash(user.password, 10)
+            }
+        })
+
+        return this
     }
 }
 
